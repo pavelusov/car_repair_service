2e6716179c21f5e561f7b0d1e6e823e70d0032eb
pavel.usov <usov.pn@gazprom-neft.ru>
2022-05-23 19:07:25 +0500

    Added a job title in the Employee class

--- index.ts
+++ index.ts
@@ -1,21 +1,23 @@
 class Employee {
     public name: string;
     private salary: number;
+    private jobTitle: string;
     
-    constructor(name: string, salary: number) {
+    constructor(name: string, salary: number, jobTitle: string) {
         this.name = name;
         this.salary = salary;
+        this.jobTitle = jobTitle;
     }
 }
 
 const employees = [
-    new Employee('Ivan', 10000),
-    new Employee('Andrew', 9000),
-    new Employee('Pavel', 8000),
-    new Employee('Vera', 11000),
-    new Employee('Mary', 7000),
-    new Employee('Gleb', 15000),
-    new Employee('Ivan', 10000),
+    new Employee('Ivan', 10000, 'Manager'),
+    new Employee('Andrew', 9000, 'Station Attendant'),
+    new Employee('Pavel', 8000, 'Station Attendant'),
+    new Employee('Vera', 11000, 'Cook'),
+    new Employee('Mary', 7000, 'Mechanic'),
+    new Employee('Gleb', 15000, 'Mechanic'),
+    new Employee('Ivan', 10000, 'Mechanic'),
 ]
 
 for (let i = 0; i < employees.length; i++) {
